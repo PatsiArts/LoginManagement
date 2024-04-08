b098b1d05659a533daac39c4d19cf223c92d7a94
Patsi <patsipatsichang@gmail.com>
2024-04-08 13:22:50 +0100

    Fix time and Add logout unit Test

---
+++ src/main/java/com/patsi/utils/DateHelper.java
@@ -0,0 +1,11 @@
+package com.patsi.utils;
+
+import java.util.Date;
+
+public class DateHelper {
+    private DateHelper() {}
+
+    public static Date getCurrentDate() {
+        return new Date();
+    }
+}
--- src/test/java/LoginServiceTest.java
+++ src/test/java/LoginServiceTest.java
@@ -6,18 +6,21 @@ import com.patsi.repository.PersonRepository;
 import com.patsi.repository.SessionRepository;
 import com.patsi.service.LogInSessionService;
 import com.patsi.service.LoginService;
+import com.patsi.utils.DateHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Date;
 import java.util.Optional;
 import java.util.UUID;
-
-import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = Main.class)
 @ActiveProfiles("test")
@@ -33,36 +36,72 @@ public class LoginServiceTest {
     @Mock
     private LogInSessionService logInSessionService;
 
-    //Constants
-    final String targetUid = "Patsi";
-    final UUID targetPersonId = UUID.randomUUID();
-    final Person targetPerson = new Person(targetPersonId, "Patsi", "Patsi",
+    private MockedStatic<DateHelper> mockDateHelper = mockStatic(DateHelper.class);
+
+    //Login
+    final String validUID = "Patsi";
+    final UUID validPersonId = UUID.randomUUID();
+    final Person validPerson = new Person(validPersonId, "Patsi", "Patsi",
         "patsi@gmail.com","PatsiSmartHome", "PatsiSmartHome");
     final LogInSession expectedLoginSession = new LogInSession();
     final String expectedToken = "test-token-123";
+    final Date currentDate = new Date();
+    //Logout
+    final String validToken = "validToken";
+    final String invalidToken = "invalidToken";
+
 
     @BeforeEach
     void setUp(){
-        when(logInSessionService.findPerson(targetUid))
+        when(logInSessionService.findPerson(validUID))
             .thenReturn(new Person());
         when(personRepository.findByUserId("Patsi"))
-            .thenReturn(Optional.of(targetPerson));
-        when(sessionRepository.findByCustomerId(targetPersonId))
+            .thenReturn(Optional.of(validPerson));
+        when(sessionRepository.findByCustomerId(validPersonId))
             .thenReturn(Optional.of(expectedLoginSession));
         when(logInSessionService.createUserToken())
             .thenReturn(expectedToken);
+        when(DateHelper.getCurrentDate()).thenReturn(currentDate);
+        when(logInSessionService.endSessionByToken(any()))
+            .thenReturn(true);
     }
 
+    //Login
     @Test
     void testCheckLogInIfValidUserIdAndPassword() {
-        UserLogin validUser = new UserLogin(targetUid, "PatsiSmartHome");
-        assertEquals("test-token-123", loginService.checkLogIn(validUser));
+        UserLogin validUser = new UserLogin(validUID, "PatsiSmartHome");
+        assertEquals(expectedToken, loginService.checkLogIn(validUser));
+        verify(logInSessionService).endSession(validPersonId);
+        verify(logInSessionService)
+            .createSession(validPersonId, expectedToken, currentDate.getTime() + 600000L);
+    }
+    @Test
+    void testCheckLogInIfInvalidUserIdAndPasswordWithNoExistingSession() {
+        when(sessionRepository.findByCustomerId(validPersonId))
+            .thenReturn(Optional.empty());
+        UserLogin user2 = new UserLogin(validUID, "PatsiSmartHome");
+        assertEquals(expectedToken, loginService.checkLogIn(user2));
+        verify(logInSessionService).findPerson(any());
+        verify(logInSessionService, never()).endSession(validPersonId);
     }
 
     @Test
     void testCheckLogInIfInvalidUserIdAndPassword() {
-        UserLogin user2 = new UserLogin("Batmy", "WrongPassword");
+        UserLogin user2 = new UserLogin("targetUid", "WrongPassword");
         assertEquals(null, loginService.checkLogIn(user2));
+        verify(logInSessionService).findPerson(any());
+        verifyNoMoreInteractions(logInSessionService);
+    }
 
+    //Logout
+    @Test
+    void testLogoutWithValidToken(){
+        assertEquals(true, loginService.logOut(validToken));
+        verify(logInSessionService).endSessionByToken(validToken);
+    }
+    @Test
+    void testLogoutWithInvalidToken(){
+        assertEquals(true, loginService.logOut(invalidToken));
+        verify(logInSessionService).endSessionByToken(invalidToken);
     }
 }
