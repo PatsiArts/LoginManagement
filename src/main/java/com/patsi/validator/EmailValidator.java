2c10954f3fb40adea4b926465f2ca21cd3e6bc88
Patsi <patsipatsichang@gmail.com>
2024-06-06 17:31:17 +0100

    Add Spring Validation and Custom valiations

---
+++ src/main/java/com/patsi/validator/EmailValidator.java
@@ -0,0 +1,33 @@
+package com.patsi.validator;
+
+import com.patsi.annotations.IsEmail;
+import com.patsi.utils.ListHelper;
+import com.patsi.utils.ValidationHelper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
+@Slf4j
+@Component
+public class EmailValidator implements Validator {
+    public Class accept() {
+        return IsEmail.class;
+    }
+
+    public List<String> validate(Object input, Map<String, Object> field){
+        log.info("In EmailValidator");
+        List<String> errorList = ListHelper.newList();
+            if (!ValidationHelper.validateValidEmail(input.toString())) {
+                errorList.add("Invalid Email!");
+                return errorList;
+            }
+        return null;
+    };
+
+}
---
+++ src/main/java/com/patsi/validator/UserNameValidation.java
@@ -0,0 +1,32 @@
+package com.patsi.validator;
+
+import com.patsi.annotations.IsUserName;
+import com.patsi.repository.ProfanityWordRepository;
+import com.patsi.utils.ValidationHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class UserNameValidation implements Validator {
+    @Autowired
+    private ProfanityWordRepository profanityWordRepository;
+
+    public Class accept() {
+        return IsUserName.class;
+    }
+
+    public List<String> validate(Object input, Map<String, Object> field) {
+        List<String> profanityWordList = profanityWordRepository.findAll().stream()
+            .map(profanityWords -> profanityWords.getWord())
+            .toList();
+        if (ValidationHelper.validateProfanity(input.toString(), profanityWordList)) {
+            return List.of();
+        } else {
+            return List.of("UserName Must not contain Profanity words!");
+        }
+    }
+}
